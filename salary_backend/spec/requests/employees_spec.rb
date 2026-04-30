93ca7842b8ec8dac571944971241fdc888d9831c
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-04-30 18:36:52 +0530

    add failing test for employee listing endpoint

--- salary_backend/spec/requests/employees_spec.rb
+++ salary_backend/spec/requests/employees_spec.rb
@@ -16,3 +16,15 @@ RSpec.describe "Employees API", type: :request do
     end
   end
 end
+
+describe "GET /employees" do
+  it "returns list of employees" do
+    Employee.create!(full_name: "Jane Doe", job_title: "HR", country: "India", salary: 40000)
+
+    get "/employees"
+
+    expect(response).to have_http_status(:ok)
+    expect(JSON.parse(response.body).length).to eq(1)
+  end
+end
+
