05762128900e24036da87cf7f0be19cc3e6955b6
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-04-30 18:44:27 +0530

    add failing test for salary insights endpoint to define expected aggregation behavior

---
+++ salary_backend/spec/requests/insights_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Insights API", type: :request do
+  describe "GET /insights" do
+    it "returns salary stats for a given country" do
+      Employee.create!(full_name: "A", job_title: "Engineer", country: "India", salary: 10000)
+      Employee.create!(full_name: "B", job_title: "Engineer", country: "India", salary: 30000)
+
+      get "/insights", params: { country: "India" }
+
+      json = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:ok)
+      expect(json["min"]).to eq(10000)
+      expect(json["max"]).to eq(30000)
+      expect(json["avg"]).to eq(20000)
+    end
+  end
+end
