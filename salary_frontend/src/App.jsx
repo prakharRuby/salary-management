16782d0d429a6abff9d679eaeeeb4a165d93439d
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-04-30 19:30:21 +0530

    display salary insights to provide summary statistics to user

--- salary_frontend/src/App.jsx
+++ salary_frontend/src/App.jsx
@@ -3,6 +3,13 @@ import api from "./services/api";
 
 function App() {
   const [employees, setEmployees] = useState([]);
+  const [insights, setInsights] = useState({});
+
+  useEffect(() => {
+    api.get("/insights", { params: { country: "India" } })
+      .then((res) => setInsights(res.data));
+  }, []);
+    
 
   useEffect(() => {
     api.get("/employees").then((res) => {
@@ -13,6 +20,12 @@ function App() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Salary Management Dashboard</h1>
+      <div>
+        <h3>Insights</h3>
+        <p>Min: {insights.min}</p>
+        <p>Max: {insights.max}</p>
+        <p>Avg: {insights.avg}</p>
+      </div>
 
       {employees.map((emp) => (
         <div key={emp.id}>
