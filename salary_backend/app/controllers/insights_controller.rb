5a85f17ecbfd30df1caaf5a597b838454ae2f8ee
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-04-30 18:51:27 +0530

    filter insights by job title to support more granular salary analysis

--- salary_backend/app/controllers/insights_controller.rb
+++ salary_backend/app/controllers/insights_controller.rb
@@ -1,2 +1,13 @@
-class InsightsController < ApplicationController
+def index
+  employees = Employee.where(country: params[:country])
+
+  if params[:job_title].present?
+    employees = employees.where(job_title: params[:job_title])
+  end
+
+  render json: {
+    min: employees.minimum(:salary),
+    max: employees.maximum(:salary),
+    avg: employees.average(:salary).to_f
+  }
 end
