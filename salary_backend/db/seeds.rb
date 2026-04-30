4052356a1550718a41a7fd8d2f0d1deadef9962e
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-04-30 19:02:08 +0530

    use provided name files to generate realistic employee data for seeding

--- salary_backend/db/seeds.rb
+++ salary_backend/db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+first_names = File.readlines(Rails.root.join("first_names.txt"), chomp: true)
+last_names  = File.readlines(Rails.root.join("last_names.txt"), chomp: true)
+
+job_titles = ["Engineer", "Manager", "HR", "Designer"]
+countries = ["India", "USA", "UK", "Canada"]
+
+10_000.times do
+  Employee.create!(
+    full_name: "#{first_names.sample} #{last_names.sample}",
+    job_title: job_titles.sample,
+    country: countries.sample,
+    salary: rand(30000..150000)
+  )
+end
