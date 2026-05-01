49db8737a03d062d88f265e9ff0d698a932798fc
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-05-02 00:42:20 +0530

    feat: root controller fixed

--- salary_backend/app/controllers/employees_controller.rb
+++ salary_backend/app/controllers/employees_controller.rb
@@ -9,27 +9,27 @@ class EmployeesController < ApplicationController
     end
   end
 
-def index
-  employees = Employee.all
+  def index
+    employees = Employee.all
 
-  if params[:search].present?
-    employees = employees.where("full_name ILIKE ?", "%#{params[:search]}%")
-  end
+    if params[:search].present?
+      employees = employees.where("full_name ILIKE ?", "%#{params[:search]}%")
+    end
 
-  employees = employees.where(country: params[:country]) if params[:country].present?
-  employees = employees.where(job_title: params[:job_title]) if params[:job_title].present?
+    employees = employees.where(country: params[:country]) if params[:country].present?
+    employees = employees.where(job_title: params[:job_title]) if params[:job_title].present?
 
-  employees = employees.page(params[:page]).per(10)
+    employees = employees.page(params[:page]).per(10)
 
-  render json: {
-    data: employees,
-    meta: {
-      page: employees.current_page,
-      per_page: 10,
-      total: employees.total_count
+    render json: {
+      data: employees,
+      meta: {
+        page: employees.current_page,
+        per_page: 10,
+        total: employees.total_count
+      }
     }
-  }
-end
+  end
 
   def show
     @employee = Employee.find_by(id:params[:id])
