9e94aeb77260c513350f44095968a87315f7d5a7
Prakhar Srivastava <prakhar.srivastava@zmail.ril.com>
2026-05-06 11:09:08 +0530

    added fresh and clean version of model validation

--- salary_backend/app/models/employee.rb
+++ salary_backend/app/models/employee.rb
@@ -1,5 +1,8 @@
 class Employee < ApplicationRecord
-    validates :salary, numericality: { greater_than: 0 }
-    validates :full_name, :job_title, :country, :salary, presence: true
     validates :email, uniqueness: true, allow_blank: true
+    validates :full_name, presence: true
+    validates :job_title, presence: true
+    validates :country, presence: true
+    validates :salary, presence: true,
+                        numericality: { greater_than: 0 }
 end
